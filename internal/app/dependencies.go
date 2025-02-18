28b5fd7c057a014b0317155dc4cfce5e68325cc8
adityapadekar-josh <aditya.padekar@joshsoftware.com>
2025-02-18 13:00:44 +0530

    Vehicleservics (#4)
    * add project setup
    
    * add user services
    
    * move db init to config dir
    
    * make redactPassword a method, move email service from pkg to app
    
    * remove comments and print statement
    
    * change naming case of constants to PascalCase
    
    * remove validation library and add basic validation functions
    
    * format db string
    
    * change code structring
    
    * change reset-password from post to patch
    
    * change config
    
    * resolve merge conflicts
    
    * change returned status to 403 for authorization
    
    * improved code structuring and error handling
    
    * move sql query statements to top of the file
    
    * remove custom error struct and handle all errors and status codes in handlers
    
    * add client_url to config and use it
    
    * update user validation function to make early returns
    
    * remove constant pkg and move content to user domain.go
    
    * make default jwt secret a global var
    
    * handle panic when retriving fields from context
    
    * make email content body constants
    
    * make token time to like constants
    
    * remove redactPassword method and add json tag to redact the password
    
    * move global and constant variables from user service.go to domain.go
    
    * restructure code
    
    * merge the signup function for seeker and host to be one
    
    * update error handling, returned errors and logging
    
    * handle duplicate key value unqiue constraint error for user creation in db layer
    
    * remove comments
    
    * change json tag is_verified to isVerified
    
    * group constants in domain.go for users
    
    * change authorization middleware to use map to check for role based access
    
    * update error handling and logging
    
    * make error logging consistent
    
    * add db transactions
    
    * add add, edit and soft delete vehicle api end to end
    
    * vehicle image structure changes and validatation updates
    
    * make error logging consistent
    
    * fix- make error logging consistent
    
    * fix error handling for create vehicle, and no update allowed if deleted

--- internal/app/dependencies.go
+++ internal/app/dependencies.go
@@ -5,20 +5,25 @@ import (
 
 	"github.com/adityapadekar-josh/Wheelio-Backend.git/internal/app/email"
 	"github.com/adityapadekar-josh/Wheelio-Backend.git/internal/app/user"
+	"github.com/adityapadekar-josh/Wheelio-Backend.git/internal/app/vehicle"
 	"github.com/adityapadekar-josh/Wheelio-Backend.git/internal/repository"
 )
 
 type Dependencies struct {
-	UserService user.Service
+	UserService    user.Service
+	VehicleService vehicle.Service
 }
 
 func NewServices(db *sql.DB) Dependencies {
 	userRepository := repository.NewUserRepository(db)
+	vehicleRepository := repository.NewVehicleRepository(db)
 
 	emailService := email.NewService()
 	userService := user.NewService(userRepository, emailService)
+	vehicleService := vehicle.NewService(vehicleRepository)
 
 	return Dependencies{
-		UserService: userService,
+		UserService:    userService,
+		VehicleService: vehicleService,
 	}
 }
---
+++ internal/app/vehicle/service.go
@@ -0,0 +1,133 @@
+package vehicle
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+
+	"github.com/adityapadekar-josh/Wheelio-Backend.git/internal/pkg/apperrors"
+	"github.com/adityapadekar-josh/Wheelio-Backend.git/internal/pkg/middleware"
+	"github.com/adityapadekar-josh/Wheelio-Backend.git/internal/repository"
+)
+
+type service struct {
+	vehicleRepository repository.VehicleRepository
+}
+
+type Service interface {
+	CreateVehicle(ctx context.Context, vehicleData VehicleWithImages) (VehicleWithImages, error)
+	UpdateVehicle(ctx context.Context, vehicleData VehicleWithImages, vehicleId int) (VehicleWithImages, error)
+	SoftDeleteVehicle(ctx context.Context, vehicleId int) (err error)
+}
+
+func NewService(vehicleRepository repository.VehicleRepository) Service {
+	return &service{
+		vehicleRepository: vehicleRepository,
+	}
+}
+
+func (s *service) CreateVehicle(ctx context.Context, vehicleData VehicleWithImages) (newVehicle VehicleWithImages, err error) {
+	userId, ok := ctx.Value(middleware.RequestContextUserIdKey).(int)
+	if !ok {
+		slog.Error("failed to retrieve user id from context")
+		return newVehicle, apperrors.ErrInternalServer
+	}
+
+	err = vehicleData.validate()
+	if err != nil {
+		slog.Error("vehicle details validation failed", "error", err)
+		return newVehicle, apperrors.ErrInvalidRequestBody
+	}
+
+	tx, err := s.vehicleRepository.BeginTx(ctx)
+	if err != nil {
+		slog.Error("failed to start user creation", "error", err)
+		return newVehicle, err
+	}
+
+	defer func() {
+		if txErr := s.vehicleRepository.HandleTransaction(ctx, tx, err); txErr != nil {
+			slog.Error("failed to handle transaction", "error", txErr)
+			err = txErr
+		}
+	}()
+
+	vehicle, err := s.vehicleRepository.CreateVehicle(ctx, tx, MapVehicleWithImagesToVehicleRepo(vehicleData), userId)
+	if err != nil {
+		slog.Error("failed to create new vehicle", "error", err)
+		return newVehicle, err
+	}
+
+	var vehicleImages []repository.VehicleImage
+	for _, vehicleImage := range vehicleData.Images {
+		updatedVehicleImage, err := s.vehicleRepository.LinkVehicleImage(ctx, tx, vehicleImage.Id, vehicle.Id)
+		if err != nil {
+			slog.Error("failed to link image with vehicle", "error", err)
+			if errors.Is(err, apperrors.ErrInvalidImageToLink) {
+				return newVehicle, apperrors.ErrInvalidRequestBody
+			}
+			return newVehicle, err
+		}
+		vehicleImages = append(vehicleImages, updatedVehicleImage)
+	}
+
+	return MapVehicleRepoAndVehicleImageRepoToVehicleWithImages(vehicle, vehicleImages), nil
+}
+
+func (s *service) UpdateVehicle(ctx context.Context, vehicleData VehicleWithImages, vehicleId int) (newVehicle VehicleWithImages, err error) {
+	err = vehicleData.validate()
+	if err != nil {
+		slog.Error("vehicle details validation failed", "error", err)
+		return newVehicle, apperrors.ErrInvalidRequestBody
+	}
+
+	tx, err := s.vehicleRepository.BeginTx(ctx)
+	if err != nil {
+		slog.Error("failed to start user creation", "error", err)
+		return newVehicle, err
+	}
+
+	defer func() {
+		if txErr := s.vehicleRepository.HandleTransaction(ctx, tx, err); txErr != nil {
+			slog.Error("failed to handle transaction", "error", txErr)
+			err = txErr
+		}
+	}()
+
+	vehicle, err := s.vehicleRepository.UpdateVehicle(ctx, tx, MapVehicleWithImagesToVehicleRepo(vehicleData), vehicleId)
+	if err != nil {
+		slog.Error("failed to update vehicle", "error", err)
+		return newVehicle, err
+	}
+
+	err = s.vehicleRepository.DeleteAllImagesForVehicle(ctx, tx, vehicleId)
+	if err != nil {
+		slog.Error("failed to delete images for vehicle", "error", err)
+		return newVehicle, err
+	}
+
+	var vehicleImages []repository.VehicleImage
+	for _, vehicleImage := range vehicleData.Images {
+		updatedVehicleImage, err := s.vehicleRepository.LinkVehicleImage(ctx, tx, vehicleImage.Id, vehicle.Id)
+		if err != nil {
+			slog.Error("failed to link image with vehicle", "error", err)
+			if errors.Is(err, apperrors.ErrInvalidImageToLink) {
+				return newVehicle, apperrors.ErrInvalidRequestBody
+			}
+			return newVehicle, err
+		}
+		vehicleImages = append(vehicleImages, updatedVehicleImage)
+	}
+
+	return MapVehicleRepoAndVehicleImageRepoToVehicleWithImages(vehicle, vehicleImages), nil
+}
+
+func (s *service) SoftDeleteVehicle(ctx context.Context, vehicleId int) (err error) {
+	err = s.vehicleRepository.SoftDeleteVehicle(ctx, nil, vehicleId)
+	if err != nil {
+		slog.Error("failed to soft delete vehicle", "error", err)
+		return err
+	}
+
+	return nil
+}
